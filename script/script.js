5e1f4094011c02acc1a666b98f8082fa955c31e8
Nikolay Dimitriev <dimitrievk56@gmail.com>
2020-06-19 15:31:50 +0300

    Выполнил домашнее задание по 12му уроку

--- script/script.js
+++ script/script.js
@@ -1,1 +1,59 @@
 'use strict'
+
+const todoControl = document.querySelector('.todo-control'),
+    headerInput = document.querySelector('.header-input'),
+    todoList = document.querySelector('.todo-list'),
+    todoCompleted = document.querySelector('.todo-completed');
+
+const todoData = JSON.parse(localStorage.getItem('todoData'));
+
+
+const render = function () {
+    todoCompleted.textContent = '';
+    todoList.textContent = '';
+
+    todoData.forEach(function (item) {
+        const li = document.createElement('li');
+        li.classList.add('todo-item');
+
+        li.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
+            '<div class="todo-buttons">' +
+            '<button class="todo-remove"></button>' +
+            '<button class="todo-complete"></button>' +
+            '</div>';
+        if (item.completed) {
+            todoCompleted.append(li);
+        } else {
+            todoList.append(li);
+        }
+
+        const btnTodoComplete = li.querySelector('.todo-complete');
+        btnTodoComplete.addEventListener('click', function () {
+            item.completed = !item.completed;
+            render();
+        });
+
+        const btnTodoRemove = li.querySelector('.todo-remove');
+        btnTodoRemove.addEventListener('click', function () {
+            todoData.splice(todoData.indexOf(item), 1);
+            render();
+        });
+    });
+    localStorage.setItem('todoData', JSON.stringify(todoData));
+};
+
+todoControl.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    if (headerInput.value !== '') {
+        const newTodo = {
+            value: headerInput.value,
+            completed: false
+        };
+        todoData.push(newTodo);
+        headerInput.value = '';
+    }
+    render();
+});
+
+render();
